764c82d24f60d60b4bcd6791ec622b9e40c97557
luiz felipe <luizfelipeeeeee13@gmail.com>
2024-06-10 18:57:07 -0300

    terceiro

--- portfolio/src/teste.tsx
+++ portfolio/src/teste.tsx
@@ -1,7 +1,32 @@
+// eslint-disable-next-line no-unused-vars
 import styled from 'styled-components'
 
+type BotaoProps = {
+  principal: boolean
+  fontSize?: string
+}
+
+const Botao = styled.button<BotaoProps>`
+  background-color: ${(props) => (props.principal ? 'green' : 'blue')};
+  font-size: ${(props) => props.fontSize || '16px'};
+`
+const BotaoPerigo = styled(Botao)`
+  background-color: red;
+  color: #fff;
+`
+
 function Teste() {
-  return <div>ola</div>
+  return (
+    <>
+      <Botao principal>Enviar</Botao>
+      <Botao fontSize="14px" principal={false}>
+        Cancelar
+      </Botao>
+      <BotaoPerigo as="a" principal>
+        Nao clique aqui
+      </BotaoPerigo>
+    </>
+  )
 }
 
 export default Teste
