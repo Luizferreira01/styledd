8ab30af2e53c4e688cd60b35aff8d724d908a97f
luiz felipe <luizfelipeeeeee13@gmail.com>
2024-06-11 20:04:47 -0300

    quinto

---
+++ portfolio/src/components/Titulo/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import { Props } from './index'
+
+export const Titulo = styled.h3<Props>`
+  color: #282a35;
+  font-size: 14px;
+  font-weight: bold;
+`
--- portfolio/src/containers/Sobre/index.tsx
+++ portfolio/src/containers/Sobre/index.tsx
@@ -2,9 +2,9 @@ import Titulo from '../../components/Titulo'
 
 const Sobre = () => (
   <section>
-    <Titulo>Sobre mim</Titulo>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
     <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam nesciunt libero veniam earum, ea quo rem natus. A nulla quam nemo facilis ea, recusandae esse voluptatum quasi ab numquam eos?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus obcaecati?
     </p>
   </section>
 )
