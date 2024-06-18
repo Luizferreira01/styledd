e0c77b200a4101ad1bd19da9269819d75cab8ad4
luiz felipe <luizfelipeeeeee13@gmail.com>
2024-06-18 20:47:26 -0300

    nono

---
+++ portfolio/src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/Luizferreira01.png" />
+
+export default Avatar
--- portfolio/src/containers/Sidebar/index.tsx
+++ portfolio/src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SiderbarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Luizferreira01.png" />
-    <Titulo fontSize={20}>Luiz Felipe</Titulo>
+    <SiderbarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Luiz Felipe</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Luizferreira01
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SiderbarContainer>
   </aside>
 )
 
