dfa159883ca4a42631f6cc9f6715cd3b455cb496
luiz felipe <luizfelipeeeeee13@gmail.com>
2024-06-18 19:56:29 -0300

    oitavo

--- portfolio/src/components/Projeto/index.tsx
+++ portfolio/src/components/Projeto/index.tsx
@@ -1,17 +1,12 @@
-import styled from 'styled-components'
-
 import Paragrafo from '../Paragrafo'
 import Titulo from '../Titulo'
-
-const Card = styled.div`
-  border: 1px solid #C1C1C1;
-  padding: 16px;
-`
+import { Card, LinkBotao } from './styles'
 
 const Projeto = () => (
   <Card>
     <Titulo>Projeto Lista De Tarefas</Titulo>
     <Paragrafo tipo="secundario">Lista De Tarefas Feita Com VueJS</Paragrafo>
+    <LinkBotao>visualizar</LinkBotao>
   </Card>
 )
 
--- portfolio/src/containers/Projetos/index.tsx
+++ portfolio/src/containers/Projetos/index.tsx
@@ -1,12 +1,37 @@
 import Projeto from '../../components/Projeto'
 import Titulo from '../../components/Titulo'
 
+import { Lista } from './styles'
+
 const Projetos = () => (
   <section>
     <Titulo fontSize={16}>Projetos</Titulo>
-    <ul>
-      <Projeto />
-    </ul>
+    <Lista>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+    </Lista>
   </section>
 )
 
